1e8eea972658cc41ba0f3c5f9ff82e980e9766ef
unknown <kenji111.fujimoto@gmail.com>
2020-01-07 15:00:08 +0100

    Updates

---
+++ precache-manifest.b3667532a1c3d991450dbf00123aad9b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "114fa82e2af996cd672a4e387962ddb8",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "b54c8b4efad3a1af287a",
+    "url": "/todolist/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "c5a8e28f5fc3ccfdf576",
+    "url": "/todolist/static/css/main.aca29cf9.chunk.css"
+  },
+  {
+    "revision": "b54c8b4efad3a1af287a",
+    "url": "/todolist/static/js/2.8556df2d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/todolist/static/js/2.8556df2d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c5a8e28f5fc3ccfdf576",
+    "url": "/todolist/static/js/main.f9a7686e.chunk.js"
+  },
+  {
+    "revision": "f2bf575a87f7a3876f48",
+    "url": "/todolist/static/js/runtime-main.64f29aa9.js"
+  }
+]);
